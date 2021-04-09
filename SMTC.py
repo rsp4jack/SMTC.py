616173abc338ee479e337cb62388b0525532cde9
Creepercdn <creepercdn@outlook.com>
2021-04-09 23:01:03 +0800

    add timeline and fix playinfo and module

--- SMTC.py
+++ SMTC.py
@@ -8,17 +8,27 @@ Returns:
 
 """
 
-from sys import platform
 import winrt
 from winrt.windows import media
 from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager, GlobalSystemMediaTransportControlsSessionPlaybackInfo as PlaybackInfo, GlobalSystemMediaTransportControlsSession as SMTCSession
+from winrt.windows.applicationmodel import AppDisplayInfo, AppInfo
+from winrt.windows.foundation import DateTime, TimeSpan, IReference
 import asyncio
 import json
+import datetime
 
 def object2dict(obj: object):
-    return {song_attr: obj.__getattribute__(song_attr) for song_attr in dir(obj) if song_attr[0] != '_'}
+    result = {song_attr: obj.__getattribute__(song_attr) for song_attr in dir(obj) if song_attr[0] != '_'}
+    for key, value in result.items():
+        if isinstance(value, TimeSpan):
+            result[key] = value.duration
+        elif isinstance(value, DateTime):
+            result[key] = value.universal_time
+        elif isinstance(value, IReference):
+            result[key] = value.value
+    return result
 
-iref2v = lambda x: x.value
+iref2v = lambda x: None if x is None else x.value
 
 async def get_smtc():
     sessions = await MediaManager.request_async()
@@ -29,15 +39,22 @@ async def get_smtc():
         media_info = object2dict(properties)
         playback_info = current_session.get_playback_info()
         playback_info = object2dict(playback_info)
+        aumid = current_session.source_app_user_model_id
+        timeline_info = current_session.get_timeline_properties()
+        timeline_info = object2dict(timeline_info)
 
-        playback_info['auto_repeat_mode'] = iref2v(playback_info.get('auto_repeat_mode'))
-        playback_info['controls'] = object2dict(playback_info.get('controls'))
-        playback_info['is_shuffle_active'] = iref2v(playback_info.get('is_shuffle_active'))
-        playback_info['playback_type'] = iref2v(playback_info.get('playback_type'))
+
+        print(type(timeline_info['max_seek_time']))
+
+        # playback_info['auto_repeat_mode'] = iref2v(playback_info.get('auto_repeat_mode'))
+        #playback_info['controls'] = object2dict(playback_info.get('controls'))
+        # playback_info['is_shuffle_active'] = iref2v(playback_info.get('is_shuffle_active'))
+        # playback_info['playback_type'] = iref2v(playback_info.get('playback_type'))
 
         genres = media_info.get('genres')
         media_info['genres'] = genres.get_many(0, genres.size)[1]
         media_info['playback'] = playback_info
+        media_info['timeline'] = timeline_info
         media_info.pop("thumbnail")
         media_info.pop("playback_type")
         return media_info
